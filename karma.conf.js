5e0ad3c6e0221516a1700b5bef901ba1ee86277e
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2022-04-28 12:48:52 +0100

    Add files via upload

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'karma-typescript'],
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    preprocessors: {
+      '**/*.ts': ['karma-typescript'],
+    },
+    plugins: ['karma-mocha', 'karma-typescript', 'karma-chrome-launcher', 'karma-firefox-launcher'],
+    karmaTypescriptConfig: {
+      tsconfig: './tsconfig.json',
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+      },
+    },
+    colors: true,
+    reporters: ['progress', 'karma-typescript'],
+    browsers: ['FirefoxHeadless', 'ChromeHeadless'],
+    singleRun: true,
+    concurrency: 1,
+    // Extend timeouts for long tests
+    browserDisconnectTimeout: 1000000,
+    browserNoActivityTimeout: 1000000,
+  })
+}
